97b448b933d61c16349009c917040b576752ebbe
SumitTarafder <sumit107756@gmail.com>
2025-10-12 21:37:03 -0400

    Update

--- setup.py
+++ setup.py
@@ -4,20 +4,33 @@ setup(
     name='PARSEbp',
     version='0.1.0',
     packages=find_packages(),
-    package_data={
-        'PARSEbp': ['bin/*'],
-    },
-    include_package_data=True,
+    include_package_data=True,   # include data files specified in MANIFEST.in
+
     install_requires=[
         'numpy',
-        'tqdm'
+        'tqdm',
     ],
+
+    package_data={
+        'PARSEbp': ['bin/*'],   # include executables inside package
+    },
+
     entry_points={
         'console_scripts': [
-            'PARSEbp = PARSEbp.cli:main',
+            'PARSEbp = PARSEbp.cli:main',   # adds CLI command
         ]
     },
+
+    author="Sumit Tarafder and Debswapna Bhattacharya",
     description="Pairwise Agreement-based RNA Scoring with Emphasis on Base Pairings",
-    author = ["Sumit Tarafder and Debswapna Bhattacharya"],
-    repository = "https://github.com/Bhattacharya-Lab/PARSEbp"
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bhattacharya-Lab/PARSEbp",
+    license="MIT",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
